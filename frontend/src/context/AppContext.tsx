375232873d314f79bce1f2aab3c6d90aff2f7eb3
Patrick <31260874+patwer92@users.noreply.github.com>
2024-11-09 13:32:31 +0100

    implemented logic to AppContext

--- frontend/src/context/AppContext.tsx
+++ frontend/src/context/AppContext.tsx
@@ -1,8 +1,23 @@
 import React, { ReactNode, useState, useEffect } from 'react'
 import { AuthContext } from './AuthContext/authContext'
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut,
+} from 'firebase/auth'
 import { auth, db, onAuthStateChanged } from '../firebase/firebase'
-import { collection, query, where, getDocs, addDoc } from 'firebase/firestore'
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  addDoc,
+  onSnapshot,
+} from 'firebase/firestore'
+import { useNavigate } from 'react-router-dom'
 
 interface AppContextProps {
   children: ReactNode
@@ -14,6 +29,8 @@ const AppContextProvider: React.FC<AppContextProps> = ({ children }) => {
   const provider = new GoogleAuthProvider()
   const [user, setUser] = useState<object | null>(null)
   const [userData, setUserData] = useState<object | null>(null)
+
+  const navigate = useNavigate
 
   const signInWithGoogle = async () => {
     try {
@@ -36,17 +53,82 @@ const AppContextProvider: React.FC<AppContextProps> = ({ children }) => {
     }
   }
 
-  loginWithUserAndEmail = async (name, email, password) => {
+  const loginWithEmailAndPassword = async (email, password) => {
     try {
-      const response = await createUse
+      await signInWithEmailAndPassword(auth, email, password)
     } catch (error) {
       alert(error)
       console.log('Error:', error)
     }
   }
 
+  const registerWithEmailAndPassword = async (name, email, password) => {
+    try {
+      const response = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      )
+      const user = response.user
+      await addDoc(collectionUserRef, {
+        uid: user.uid,
+        name: name,
+        email: user.email,
+        providerId: 'email/password',
+      })
+    } catch (error) {
+      console.log('Error:', error)
+    }
+  }
+
+  const sendPasswordToUser = async (email) => {
+    try {
+      await sendPasswordResetEmail(auth, email).then((res) => {
+        console.log('Email sent:', res)
+      })
+      alert('Nytt passord er sendt til din e-postadresse')
+    } catch (error) {
+      console.log('Error:', error)
+    }
+  }
+
+  const signOutUser = async () => {
+    await signOut(auth)
+  }
+
+  const userStateChanged = async () => {
+    onAuthStateChanged(auth, async () => {
+      if (user) {
+        const q = query(collectionUserRef, where('uid', '==', user?.uid))
+        onSnapshot(q, (doc) => {
+          setUserData(doc?.docs[0]?.data())
+        })
+        setUser(user)
+      } else {
+        setUser(null)
+        navigate('/login')
+      }
+    })
+  }
+
+  useEffect(() => {
+    userStateChanged()
+    if (user || userData) {
+      navigate('/')
+    } else {
+      navigate('/login')
+    }
+    return () => userStateChanged()
+  }, [])
+
   const initialState = {
     signInWithGoogle: signInWithGoogle,
+    loginWithEmailAndPassword: loginWithEmailAndPassword,
+    registerWithEmailAndPassword: registerWithEmailAndPassword,
+    sendPasswordToUser: sendPasswordToUser,
+    signOutUser: signOutUser,
+    user: user,
+    userData: userData,
   }
 
   return (
