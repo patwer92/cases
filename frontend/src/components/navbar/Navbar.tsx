24085d905a72fc35cb7b0f27ac32641c9e877979
Patrick <31260874+patwer92@users.noreply.github.com>
2024-12-03 10:02:18 +0100

    Renamed 'Nyhetsfeed' to 'Sosiale Medier' in the Navbar component

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar: React.FC = (): React.ReactElement => {
       <Link to='/'>
         <div className='text-2xl font-extrabold text-gray-900 dark:text-white font-roboto'>
           <span className='bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-blue-800'>
-            Nyhetsfeed
+            Sosiale Medier
           </span>{' '}
           App
         </div>
